e5de79bd7e55eb8bb91eaf0128517310e060742e
MaksKulikov <kulykov.maks@gmail.com>
2017-02-28 12:51:56 +0200

    added a list of gadgets

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -18,21 +18,90 @@ exports.Gadget = Gadget;
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Gadgets Catalog';
+        this.gadgets = GADGETS;
         this.gadget = {
             gadgetName: 'HTC',
-            gadgetPrice: '2222',
+            gadgetPrice: 2222,
             gadgetPopular: true,
-            gadgetDate: '1467440203727'
+            gadgetDate: 1467440203727
         };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n          <h1>{{title}}</h1>\n          <div class=\"row\">\n            <div class=\"col-lg-6\">\n              <div class=\"thumbnail\">\n                <div class=\"caption\">\n                  <p><b>\u041D\u0430\u0437\u0432\u0430\u043D\u0438\u0435:</b>     {{gadget.gadgetName}} </p>\n                  <p><b>\u0426\u0435\u043D\u0430:</b>         {{gadget.gadgetPrice}} \u0433\u0440\u043D</p>\n                  <p><b>\u041F\u043E\u043F\u0443\u043B\u044F\u0440\u043D\u043E\u0441\u0442\u044C:</b> {{gadget.gadgetPopular | modifyPopular}}</p>\n                  <p><b>\u0414\u0430\u0442\u0430:</b>         {{gadget.gadgetDate | date:\"dd/MM/yyyy\"}}</p>\n                </div>\n              </div>\n            </div>\n            <div class=\"col-lg-2\">\n              <div class=\"btn btn-default\" data-class=\"btnEdit\">\u0420\u0435\u0434\u0430\u043A\u0442\u0438\u0440\u043E\u0432\u0430\u0442\u044C</div>\n            </div>\n           </div>"
+            template: "\n          <h1>{{title}}</h1>\n          <div class=\"row\"  *ngFor=\"let gadget of gadgets\">\n            <div class=\"col-lg-6\">\n              <div class=\"thumbnail\">\n                <div class=\"caption\">\n                  <div><label>\u041D\u0430\u0437\u0432\u0430\u043D\u0438\u0435:</label>     {{gadget.gadgetName}} </div>\n                  <div><label>\u0426\u0435\u043D\u0430:</label>         {{gadget.gadgetPrice}} \u0433\u0440\u043D</div>\n                  <div><label>\u041F\u043E\u043F\u0443\u043B\u044F\u0440\u043D\u043E\u0441\u0442\u044C:</label> {{gadget.gadgetPopular | modifyPopular}}</div>\n                  <div><label>\u0414\u0430\u0442\u0430:</label>        {{gadget.gadgetDate | date:\"dd/MM/yyyy\"}}</div>\n                </div>\n              </div>\n            </div>\n            <div class=\"col-lg-2\">\n              <div class=\"btn btn-default\" data-class=\"btnEdit\">\u0420\u0435\u0434\u0430\u043A\u0442\u0438\u0440\u043E\u0432\u0430\u0442\u044C</div>\n            </div>\n           </div>"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+var GADGETS = [
+    {
+        "gadgetName": "HTC",
+        "gadgetPrice": 12458,
+        "gadgetPopular": true,
+        "gadgetDate": 1467440203
+    },
+    {
+        "gadgetName": "Sony",
+        "gadgetPrice": 12458,
+        "gadgetPopular": true,
+        "gadgetDate": 1467440207
+    },
+    {
+        "gadgetName": "HTC",
+        "gadgetPrice": 12458,
+        "gadgetPopular": true,
+        "gadgetDate": 146744727
+    },
+    {
+        "gadgetName": "HTC",
+        "gadgetPrice": 12458,
+        "gadgetPopular": true,
+        "gadgetDate": 1467440227
+    },
+    {
+        "gadgetName": "Sony",
+        "gadgetPrice": 32458,
+        "gadgetPopular": false,
+        "gadgetDate": 14674402037
+    },
+    {
+        "gadgetName": "Sony",
+        "gadgetPrice": 32458,
+        "gadgetPopular": false,
+        "gadgetDate": 146744020327
+    },
+    {
+        "gadgetName": "Sony",
+        "gadgetPrice": 12458,
+        "gadgetPopular": false,
+        "gadgetDate": 1467440207
+    },
+    {
+        "gadgetName": "HTC",
+        "gadgetPrice": 12458,
+        "gadgetPopular": true,
+        "gadgetDate": 14644003727
+    },
+    {
+        "gadgetName": "HTC",
+        "gadgetPrice": 12458,
+        "gadgetPopular": true,
+        "gadgetDate": 146744203727
+    },
+    {
+        "gadgetName": "HTC",
+        "gadgetPrice": 12458,
+        "gadgetPopular": true,
+        "gadgetDate": 1467440203727
+    },
+    {
+        "gadgetName": "HTC",
+        "gadgetPrice": 12458,
+        "gadgetPopular": true,
+        "gadgetDate": 1467440203727
+    }
+];
 //# sourceMappingURL=app.component.js.map
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,23 @@ import { Component } from '@angular/core';
 
 export class Gadget {
   gadgetName: string;
-  gadgetPrice: string;
+  gadgetPrice: number;
   gadgetPopular: boolean;
-  gadgetDate: string;
+  gadgetDate: number;
 }
 
 @Component({
   selector: 'my-app',
   template: `
           <h1>{{title}}</h1>
-          <div class="row">
+          <div class="row"  *ngFor="let gadget of gadgets">
             <div class="col-lg-6">
               <div class="thumbnail">
                 <div class="caption">
-                  <p><b>Название:</b>     {{gadget.gadgetName}} </p>
-                  <p><b>Цена:</b>         {{gadget.gadgetPrice}} грн</p>
-                  <p><b>Популярность:</b> {{gadget.gadgetPopular | modifyPopular}}</p>
-                  <p><b>Дата:</b>         {{gadget.gadgetDate | date:"dd/MM/yyyy"}}</p>
+                  <div><label>Название:</label>     {{gadget.gadgetName}} </div>
+                  <div><label>Цена:</label>         {{gadget.gadgetPrice}} грн</div>
+                  <div><label>Популярность:</label> {{gadget.gadgetPopular | modifyPopular}}</div>
+                  <div><label>Дата:</label>        {{gadget.gadgetDate | date:"dd/MM/yyyy"}}</div>
                 </div>
               </div>
             </div>
@@ -30,10 +30,80 @@ export class Gadget {
 
 export class AppComponent {
   title = 'Gadgets Catalog';
+  gadgets = GADGETS;
   gadget: Gadget = {
     gadgetName: 'HTC',
-    gadgetPrice: '2222',
+    gadgetPrice: 2222,
     gadgetPopular: true,
-    gadgetDate: '1467440203727'
+    gadgetDate: 1467440203727
   };
 }
+
+const GADGETS: Gadget[] = [
+  {
+    "gadgetName": "HTC",
+    "gadgetPrice": 12458,
+    "gadgetPopular": true,
+    "gadgetDate": 1467440203
+  },
+  {
+    "gadgetName": "Sony",
+    "gadgetPrice": 12458,
+    "gadgetPopular": true,
+    "gadgetDate": 1467440207
+  },
+  {
+    "gadgetName": "HTC",
+    "gadgetPrice": 12458,
+    "gadgetPopular": true,
+    "gadgetDate": 146744727
+  },
+  {
+    "gadgetName": "HTC",
+    "gadgetPrice": 12458,
+    "gadgetPopular": true,
+    "gadgetDate": 1467440227
+  },
+  {
+    "gadgetName": "Sony",
+    "gadgetPrice": 32458,
+    "gadgetPopular": false,
+    "gadgetDate": 14674402037
+  },
+  {
+    "gadgetName": "Sony",
+    "gadgetPrice": 32458,
+    "gadgetPopular": false,
+    "gadgetDate": 146744020327
+  },
+  {
+    "gadgetName": "Sony",
+    "gadgetPrice": 12458,
+    "gadgetPopular": false,
+    "gadgetDate": 1467440207
+  },
+  {
+    "gadgetName": "HTC",
+    "gadgetPrice": 12458,
+    "gadgetPopular": true,
+    "gadgetDate": 14644003727
+  },
+  {
+    "gadgetName": "HTC",
+    "gadgetPrice": 12458,
+    "gadgetPopular": true,
+    "gadgetDate": 146744203727
+  },
+  {
+    "gadgetName": "HTC",
+    "gadgetPrice": 12458,
+    "gadgetPopular": true,
+    "gadgetDate": 1467440203727
+  },
+  {
+    "gadgetName": "HTC",
+    "gadgetPrice": 12458,
+    "gadgetPopular": true,
+    "gadgetDate": 1467440203727
+  }
+];
